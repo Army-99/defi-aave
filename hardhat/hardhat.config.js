15f7c633a7d0e24c9bf76bc67faf5ec09ba145a8
Chris3rr <101435102+Chris3rr@users.noreply.github.com>
2022-09-18 18:22:40 +0200

    borrowing and repaying functions

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -11,39 +11,21 @@ const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
-        /*
-        localhost: {
-            chainId: 31337,
-            blockConfirmations: 1
-        },
-        goerli: {
-            chainId: 5,
-            blockConfirmations: 6,
-            url: GOERLI_RPC_URL,
-            accounts: [PRIVATE_KEY]
-        },
-        mumbai: {
-            chainId: 80001,
-            blockConfirmations: 6,
-            url: MUMBAI_RPC_URL,
-            accounts: [PRIVATE_KEY]
-        },
-        bsctestnet: {
-            chainId: 97,
-            blockConfirmations: 6,
-            url: BSC_RPC_URL,
-            accounts: [PRIVATE_KEY]
-        },
-        */
         hardhat: {
             forking: {
             url: MAINNET_RPC_URL
             }
         }
     },
-    //solidity: "0.8.7",
     solidity: {
-        compilers: [{ version: "0.8.7" }, { version: "0.6.6" }, { version: "0.4.19" }]
+        compilers: [
+             { version: "0.8.7"}
+            ,{ version: "0.6.12"}
+            ,{ version: "0.6.6"}
+            ,{ version: "0.6.0"}
+            ,{ version: "0.4.19"}
+            ,{ version: "0.6.0" }
+        ]
     },
     namedAccounts: {
         deployer: {
@@ -64,5 +46,5 @@ module.exports = {
           bscTestnet: BSCSCAN_API_KEY
         }
     }
-*/
+    */
 };
--- hardhat/scripts/getWeth.js
+++ hardhat/scripts/getWeth.js
@@ -6,14 +6,11 @@ const getWeth = async () => {
     const {deployer} = await getNamedAccounts();
     //DEPOSIT FUNCTION IN CONTRACT
     //address: 0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6
-    //abi
-
     const iWeth = await ethers.getContractAt("IWeth","0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", deployer);
-
     const tx = await iWeth.deposit({value: AMOUNT});
     await tx.wait(1);
     const wethBalance = await iWeth.balanceOf(deployer);
-    console.log(wethBalance.toString());
+    console.log(`Balance WETH: ${ethers.utils.formatEther(wethBalance.toString())}`);
 }
 
-module.exports = { getWeth };
+module.exports = { getWeth, AMOUNT };
