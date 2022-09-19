e117bf6cb1915a96c4a2b29b6513c8efa348ed54
Army-99 <101435102+Army-99@users.noreply.github.com>
2022-09-19 14:18:17 +0200

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,12 +1,15 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
 require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-deploy")
 
-const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -14,38 +17,39 @@ module.exports = {
         hardhat: {
             chainId: 31337,
             forking: {
-            url: MAINNET_RPC_URL
-            }
-        }
+                url: MAINNET_RPC_URL,
+            },
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
     },
     solidity: {
         compilers: [
-             { version: "0.8.7"}
-            ,{ version: "0.6.12"}
-            ,{ version: "0.6.6"}
-            ,{ version: "0.6.0"}
-            ,{ version: "0.4.19"}
-            ,{ version: "0.6.0" }
-        ]
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.6.12",
+            },
+            {
+                version: "0.4.19",
+            },
+        ],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
     namedAccounts: {
         deployer: {
             default: 0,
-        },
-        player: {
-            default: 1,
+            1: 0,
         },
     },
-    mocha: {
-        timeout: 200000
-    },
-    /*
-    etherscan: {
-        apiKey: {
-          polygonMumbai: MUMBAI_API,
-          goerli: ETHERSCAN_API,
-          bscTestnet: BSCSCAN_API_KEY
-        }
-    }
-    */
-};
+}
